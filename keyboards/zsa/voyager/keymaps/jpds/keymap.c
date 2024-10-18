886320cf8ef19ad2c9a32f3f63bface2e44000bc
Jonathan Davies <jpds@protonmail.com>
2024-10-18 21:09:31 +0100

    Added brightness keys

--- keyboards/zsa/voyager/keymaps/jpds/keymap.c
+++ keyboards/zsa/voyager/keymaps/jpds/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                      _______, _______,    _______, KC_0
     ),
     [NAV] = LAYOUT(
-        RGB_TOG, QK_KB,   RGB_MOD, RGB_M_P, RGB_VAD, RGB_VAI,                      _______, _______, _______, _______, _______, QK_BOOT,
+        RGB_TOG, QK_KB,   RGB_MOD, RGB_M_P, RGB_VAD, RGB_VAI,                      KC_BRIU, KC_BRID, _______, _______, _______, QK_BOOT,
         _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______,
         _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______,                      KC_DEL, C(S(KC_TAB)), C(KC_TAB), KC_PSCR, _______, _______,
