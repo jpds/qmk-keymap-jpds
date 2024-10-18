64df707dd978097309d7dcc25ddcab3a0a045bf5
Jonathan Davies <jpds@protonmail.com>
2024-10-18 22:30:06 +0100

    Switched to Colemak-DH

--- keyboards/zsa/voyager/keymaps/jpds/keymap.c
+++ keyboards/zsa/voyager/keymaps/jpds/keymap.c
@@ -31,9 +31,9 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
         TD(TD_ESC_EQL), KC_1, KC_2, KC_3,   KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        CW_TOGG, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        SFT_T(KC_BSPC),KC_A, KC_S, KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RSFT_T(KC_QUOT),
-        KC_LGUI, ALT_T(KC_Z),KC_X, KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMMA,KC_DOT,  RALT_T(KC_SLSH), KC_HYPR,
+        CW_TOGG, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+        SFT_T(KC_BSPC),KC_A, KC_R, KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, RSFT_T(KC_QUOT),
+        KC_LGUI, ALT_T(KC_Z),KC_X, KC_C,    KC_D,    KC_V,                         KC_K,    KC_H,    KC_COMMA,KC_DOT,  RALT_T(KC_SLSH), KC_HYPR,
                                 LT(SYM,KC_ENT), CTL_T(KC_TAB),           SFT_T(KC_BSPC), LT(NAV,KC_SPC)
     ),
     [SYM] = LAYOUT(
