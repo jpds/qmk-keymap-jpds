6a3189bf0b7f93d9887b280cd1d4f0306994d692
Jonathan Davies <jpds@protonmail.com>
2024-10-18 20:47:19 +0100

    Added semicolon combo

--- keyboards/zsa/voyager/keymaps/jpds/keymap.c
+++ keyboards/zsa/voyager/keymaps/jpds/keymap.c
@@ -10,6 +10,12 @@ enum layers {
   NAV
 };
 
+// Combos
+const uint16_t PROGMEM semicolon_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(semicolon_combo, KC_SCLN),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
