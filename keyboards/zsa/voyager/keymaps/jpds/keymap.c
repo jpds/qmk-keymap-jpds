3ddb59c0e49d118c51e06b2fefd67d304c316c14
Jonathan Davies <jpds@protonmail.com>
2025-12-30 14:36:27 +0000

    keymap.c: Run `qmkfmt`

--- keyboards/zsa/voyager/keymaps/jpds/keymap.c
+++ keyboards/zsa/voyager/keymaps/jpds/keymap.c
@@ -4,17 +4,11 @@
 
 #include QMK_KEYBOARD_H
 
-enum layers {
-  BASE,
-  SYM,
-  NAV,
-  STENO,
-  MOUSE
-};
+enum layers { BASE, SYM, NAV, STENO, MOUSE };
 
 // Combos
 const uint16_t PROGMEM semicolon_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
-combo_t key_combos[] = {
+combo_t                key_combos[]      = {
     COMBO(semicolon_combo, KC_SCLN),
 };
 
@@ -29,105 +23,106 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_ESC_EQL] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_EQL),
 };
 
-const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
-    LAYOUT(
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-                            '*', '*',  '*', '*'
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
+        'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+                            '*', '*', '*', '*'
     );
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [BASE] = LAYOUT(
-        TD(TD_ESC_EQL), KC_1, KC_2, KC_3,   KC_4,    KC_5,                                                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        CW_TOGG, KC_B,    KC_L,    KC_D,    KC_C,    KC_V,                                                    KC_J,    KC_Y,    KC_O,    KC_U,    KC_COMMA,    KC_BSLS,
-        OSM(MOD_LSFT), MT(MOD_LGUI, KC_N), MT(MOD_LALT, KC_R), MT(MOD_LSFT, KC_T), MT(MOD_LCTL, KC_S), KC_G,  KC_P,    MT(MOD_RCTL, KC_H),    MT(MOD_RSFT, KC_A),    MT(MOD_RALT, KC_E), MT(MOD_RGUI, KC_I), RSFT_T(KC_SLSH),
-        KC_LGUI, ALT_T(KC_X), KC_Q, KC_M,   KC_W,    KC_Z,                                                    KC_K,    KC_F,    KC_QUOT, KC_SCLN, KC_DOT, KC_HYPR,
-                                LT(SYM,KC_ENT), CTL_T(KC_TAB),                                             SFT_T(KC_BSPC), LT(NAV,KC_SPC)
+    [BASE]  = LAYOUT(
+        TD(TD_ESC_EQL), KC_1,               KC_2,               KC_3,               KC_4,               KC_5,          KC_6,           KC_7,               KC_8,               KC_9,               KC_0,               KC_MINS,
+        CW_TOGG,        KC_B,               KC_L,               KC_D,               KC_C,               KC_V,          KC_J,           KC_Y,               KC_O,               KC_U,               KC_COMMA,           KC_BSLS,
+        OSM(MOD_LSFT),  MT(MOD_LGUI, KC_N), MT(MOD_LALT, KC_R), MT(MOD_LSFT, KC_T), MT(MOD_LCTL, KC_S), KC_G,          KC_P,           MT(MOD_RCTL, KC_H), MT(MOD_RSFT, KC_A), MT(MOD_RALT, KC_E), MT(MOD_RGUI, KC_I), RSFT_T(KC_SLSH),
+        KC_LGUI,        ALT_T(KC_X),        KC_Q,               KC_M,               KC_W,               KC_Z,          KC_K,           KC_F,               KC_QUOT,            KC_SCLN,            KC_DOT,             KC_HYPR,
+                                                                                    LT(SYM,KC_ENT),     CTL_T(KC_TAB), SFT_T(KC_BSPC), LT(NAV,KC_SPC)
     ),
-    [SYM] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_MINS, KC_7, KC_8, KC_9, KC_SLSH, KC_F12,
-        _______, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, _______,                      KC_PLUS, KC_4, KC_5, KC_6, KC_ASTR, _______,
-        TO(STENO), KC_RABK, KC_RBRC, KC_RCBR, KC_RPRN, _______,                    _______, KC_1, KC_2, KC_3, _______, KC_ENT,
-                                                     _______, _______,    _______, KC_0
+    [SYM]   = LAYOUT(
+        KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,  KC_F11,
+        KC_GRV,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_MINS, KC_7,  KC_8,  KC_9,  KC_SLSH, KC_F12,
+        _______,   KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, _______, KC_PLUS, KC_4,  KC_5,  KC_6,  KC_ASTR, _______,
+        TO(STENO), KC_RABK, KC_RBRC, KC_RCBR, KC_RPRN, _______, _______, KC_1,  KC_2,  KC_3,  _______, KC_ENT,
+                                              _______, _______, _______, KC_0
     ),
-    [NAV] = LAYOUT(
-        RM_TOGG, QK_KB,   RM_NEXT, RGB_M_P, RM_VALD, RM_VALU,                      KC_BRID, KC_BRIU, _______, _______, _______, QK_BOOT,
-        RM_HUEU, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,                      KC_PGUP, KC_HOME, KC_DEL,  KC_END,  _______, KC_SLEP,
-        LUMINO,  KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, _______, _______, _______, _______, _______,                      _______, C(S(KC_TAB)), C(KC_TAB), KC_PSCR, _______, TO(MOUSE),
-                                                     _______, _______,    _______, _______
+    [NAV]   = LAYOUT(
+        RM_TOGG, QK_KB,   RM_NEXT, RGB_M_P, RM_VALD, RM_VALU, KC_BRID, KC_BRIU,      _______,   _______, _______, QK_BOOT,
+        RM_HUEU, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_PGUP, KC_HOME,      KC_DEL,    KC_END,  _______, KC_SLEP,
+        LUMINO,  KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______, KC_PGDN, KC_LEFT,      KC_DOWN,   KC_UP,   KC_RGHT, _______,
+        _______, _______, _______, _______, _______, _______, _______, C(S(KC_TAB)), C(KC_TAB), KC_PSCR, _______, TO(MOUSE),
+                                            _______, _______, _______, _______
     ),
     [STENO] = LAYOUT(
-        _______, STN_N1, STN_N2, STN_N3, STN_N4, STN_N5,                           STN_N6,  STN_N7, STN_N8, STN_N9, STN_NA, STN_NB,
-        _______, STN_S1, STN_TL, STN_PL, STN_HL, STN_ST1,                          STN_ST1, STN_FR, STN_PR, STN_LR, STN_TR, STN_DR,
-        _______, STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2,                          STN_ST4, STN_RR, STN_BR, STN_GR, STN_SR, STN_ZR,
-        TO(BASE), _______, _______, _______, STN_NC, QK_STENO_BOLT,                QK_STENO_GEMINI, STN_NC, STN_FN, STN_RES1, STN_RES2, STN_PWR,
-                                                     STN_A, STN_O,        STN_E, STN_U
+        _______,  STN_N1,  STN_N2,  STN_N3,  STN_N4, STN_N5,        STN_N6,          STN_N7, STN_N8, STN_N9,   STN_NA,   STN_NB,
+        _______,  STN_S1,  STN_TL,  STN_PL,  STN_HL, STN_ST1,       STN_ST1,         STN_FR, STN_PR, STN_LR,   STN_TR,   STN_DR,
+        _______,  STN_S2,  STN_KL,  STN_WL,  STN_RL, STN_ST2,       STN_ST4,         STN_RR, STN_BR, STN_GR,   STN_SR,   STN_ZR,
+        TO(BASE), _______, _______, _______, STN_NC, QK_STENO_BOLT, QK_STENO_GEMINI, STN_NC, STN_FN, STN_RES1, STN_RES2, STN_PWR,
+                                             STN_A,  STN_O,         STN_E,           STN_U
     ),
     [MOUSE] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                      _______,  _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                      _______,  _______, _______, _______, _______, _______,
-        _______, MS_BTN1, MS_WHLU, MS_WHLD, MS_BTN2, _______,                      _______,  MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, _______,
-        TO(BASE), _______, _______, _______, _______, _______,                      _______,  _______, _______, _______, _______, _______,
-                                                     _______, _______,    _______, _______
+        _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,  MS_BTN1, MS_WHLU, MS_WHLD, MS_BTN2, _______, _______, MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, _______,
+        TO(BASE), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                                             _______, _______, _______, _______
     ),
 };
 
 extern rgb_config_t rgb_matrix_config;
 
 void keyboard_post_init_user(void) {
-  rgb_matrix_enable();
+    rgb_matrix_enable();
 }
 
 const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
-    [SYM]   =   { {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_MAGENTA}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_ORANGE}, {HSV_RED}, {HSV_GREEN}, {HSV_OFF}, {HSV_WHITE}, {HSV_CYAN}, {HSV_ORANGE}, {HSV_RED}, {HSV_GREEN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_ORANGE}, {HSV_GREEN}, {HSV_GREEN}, {HSV_GREEN}, {HSV_ORANGE}, {HSV_OFF}, {HSV_ORANGE}, {HSV_GREEN}, {HSV_GREEN}, {HSV_GREEN}, {HSV_ORANGE}, {HSV_OFF}, {HSV_OFF}, {HSV_GREEN}, {HSV_GREEN}, {HSV_GREEN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_GREEN} },
-    [NAV]   =   { {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_GREEN}, {HSV_GREEN}, {HSV_RED}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_GREEN}, {HSV_GREEN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_RED}, {HSV_OFF}, {HSV_ORANGE}, {HSV_RED}, {HSV_ORANGE}, {HSV_OFF}, {HSV_YELLOW}, {HSV_OFF}, {HSV_SPRINGGREEN}, {HSV_SPRINGGREEN}, {HSV_SPRINGGREEN}, {HSV_SPRINGGREEN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_MAGENTA}, {HSV_OFF}, {HSV_OFF} },
-    [STENO] =   { {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_WHITE}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN} },
-    [MOUSE]   = { {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_AZURE}, {HSV_GREEN}, {HSV_GREEN}, {HSV_AZURE}, {HSV_OFF}, {HSV_WHITE}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_MAGENTA}, {HSV_MAGENTA}, {HSV_MAGENTA}, {HSV_MAGENTA}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF} },
+    [SYM]   = {{HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_MAGENTA}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_ORANGE}, {HSV_RED}, {HSV_GREEN}, {HSV_OFF}, {HSV_WHITE}, {HSV_CYAN}, {HSV_ORANGE}, {HSV_RED}, {HSV_GREEN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_ORANGE}, {HSV_GREEN}, {HSV_GREEN}, {HSV_GREEN}, {HSV_ORANGE}, {HSV_OFF}, {HSV_ORANGE}, {HSV_GREEN}, {HSV_GREEN}, {HSV_GREEN}, {HSV_ORANGE}, {HSV_OFF}, {HSV_OFF}, {HSV_GREEN}, {HSV_GREEN}, {HSV_GREEN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_GREEN}},
+    [NAV]   = {{HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_GREEN}, {HSV_GREEN}, {HSV_RED}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_GREEN}, {HSV_GREEN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_RED}, {HSV_OFF}, {HSV_ORANGE}, {HSV_RED}, {HSV_ORANGE}, {HSV_OFF}, {HSV_YELLOW}, {HSV_OFF}, {HSV_SPRINGGREEN}, {HSV_SPRINGGREEN}, {HSV_SPRINGGREEN}, {HSV_SPRINGGREEN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_MAGENTA}, {HSV_OFF}, {HSV_OFF}},
+    [STENO] = {{HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_WHITE}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_CYAN}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_CYAN}, {HSV_CYAN}},
+    [MOUSE] = {{HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_AZURE}, {HSV_GREEN}, {HSV_GREEN}, {HSV_AZURE}, {HSV_OFF}, {HSV_WHITE}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_MAGENTA}, {HSV_MAGENTA}, {HSV_MAGENTA}, {HSV_MAGENTA}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}, {HSV_OFF}},
 };
 
 void set_layer_color(int layer) {
-  for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
-    HSV hsv = {
-      .h = pgm_read_byte(&ledmap[layer][i][0]),
-      .s = pgm_read_byte(&ledmap[layer][i][1]),
-      .v = pgm_read_byte(&ledmap[layer][i][2]),
-    };
-    if (!hsv.h && !hsv.s && !hsv.v) {
-        rgb_matrix_set_color( i, HSV_OFF );
-    } else {
-        RGB rgb = hsv_to_rgb( hsv );
-        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
+    for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+        HSV hsv = {
+            .h = pgm_read_byte(&ledmap[layer][i][0]),
+            .s = pgm_read_byte(&ledmap[layer][i][1]),
+            .v = pgm_read_byte(&ledmap[layer][i][2]),
+        };
+        if (!hsv.h && !hsv.s && !hsv.v) {
+            rgb_matrix_set_color(i, HSV_OFF);
+        } else {
+            RGB   rgb = hsv_to_rgb(hsv);
+            float f   = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+            rgb_matrix_set_color(i, f * rgb.r, f * rgb.g, f * rgb.b);
+        }
     }
-  }
 }
 
 bool rgb_matrix_indicators_user(void) {
-  if (keyboard_config.disable_layer_led) { return false; }
-  switch (biton32(layer_state)) {
-    // case 0:
-    //   set_layer_color(0);
-    //   break;
-    case 1:
-      set_layer_color(1);
-      break;
-    case 2:
-      set_layer_color(2);
-      break;
-    case 3:
-      set_layer_color(3);
-      break;
-    case 4:
-      set_layer_color(4);
-      break;
-   default:
-    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
-      rgb_matrix_set_color_all(HSV_OFF);
-    break;
-  }
-  return true;
+    if (keyboard_config.disable_layer_led) {
+        return false;
+    }
+    switch (biton32(layer_state)) {
+        // case 0:
+        //   set_layer_color(0);
+        //   break;
+        case 1:
+            set_layer_color(1);
+            break;
+        case 2:
+            set_layer_color(2);
+            break;
+        case 3:
+            set_layer_color(3);
+            break;
+        case 4:
+            set_layer_color(4);
+            break;
+        default:
+            if (rgb_matrix_get_flags() == LED_FLAG_NONE) rgb_matrix_set_color_all(HSV_OFF);
+            break;
+    }
+    return true;
 }
+
