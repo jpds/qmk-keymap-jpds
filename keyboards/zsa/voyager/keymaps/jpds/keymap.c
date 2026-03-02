234ccb0d028ccdaa3bcecf726bc0014ee8be122b
Jonathan Davies <jpds@protonmail.com>
2026-03-02 15:07:56 +0000

    keymap.c: Remove unnecessary `rgb_matrix_config`
    Already declared in QMK's `rgb_matrix.h`

--- keyboards/zsa/voyager/keymaps/jpds/keymap.c
+++ keyboards/zsa/voyager/keymaps/jpds/keymap.c
@@ -69,8 +69,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-extern rgb_config_t rgb_matrix_config;
-
 void keyboard_post_init_user(void) {
     rgb_matrix_enable();
 }
